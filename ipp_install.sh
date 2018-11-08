4460aeee362985d1fdf749a6cbd4339978ac3e0e
Andrei Nigmatulin <andrei.nigmatulin@gmail.com>
2018-11-08 01:53:10 +0000

    minor fixes to ipp_install.sh

--- ipp_install.sh
+++ ipp_install.sh
@@ -1,16 +1,22 @@
-#! /bin/sh
+#! /bin/bash
 
-rpm --nodeps -Uvh \
+sudo rpm --nodeps -Uvh \
 	intel-ipp-common-2018.4-274-2018.4-274.noarch.rpm \
 	intel-ipp-st-2018.4-274-2018.4-274.x86_64.rpm \
 	intel-ipp-st-devel-2018.4-274-2018.4-274.x86_64.rpm
 
 ipp_root="/opt/intel/compilers_and_libraries_2018.5.274/linux/ipp"
 
-echo PKG_CONFIG_PATH="${ipp_root}/lib/intel64_lin/pkgconfig:\$PKG_CONFIG_PATH" >> /etc/profile
+sudo install -D -m 644 ippi.pc "${ipp_root}/lib/intel64_lin/pkgconfig/"
 
-install -d -m 644 ippi.pc ${ipp_root}/lib/intel64_lin/pkgconfig/
+echo "${ipp_root}/lib/intel64_lin" | sudo tee /etc/ld.so.conf.d/ipp.conf
 
-echo "${ipp_root}/lib/intel64_lin" > /etc/ld.so.conf.d/ipp.conf
+sudo /sbin/ldconfig
 
-/sbin/ldconfig
+pip3 install --user git+https://github.com/anight/pkg-config-force-static.git
+
+echo "export PKG_CONFIG_PATH=\"${ipp_root}/lib/intel64_lin/pkgconfig:\$PKG_CONFIG_PATH\"" >> ~/.bash_profile
+
+# use this if you want to link with ipp libs statically
+echo "export PKG_CONFIG_FORCE_STATIC=\"ippi\"" >> ~/.bash_profile
+
